7e1177fef9cc0e0f9f1dfc0fb7e0c44f30f3e8b5
Danulo <danilyanishevski2001@gmail.com>
2024-02-08 15:49:29 +0200

    70%finish

--- src/main.js
+++ src/main.js
@@ -2,9 +2,22 @@ import axios from 'axios';
 
 const filterButtons = document.querySelector('.filter-buttons');
 const exerciseFiltersList = document.querySelector('.exercise-filters-list');
+const pagination = document.querySelector('.pagination');
 const BASE_URL = 'https://energyflow.b.goit.study/api';
-const filterValueDefault = 'Muscles';
+let filterValueDefault = 'Muscles';
+let currentPage = 1;
+let screenWidth = window.innerWidth;
+let currentLimit = 0;
+
 filterButtons.addEventListener('click', filterBtnClick);
+
+if (screenWidth <= 375) {
+  currentLimit = 8;
+} else if (screenWidth <= 768) {
+  currentLimit = 12;
+} else {
+  currentLimit = 12;
+}
 
 async function filterBtnClick(event) {
   event.preventDefault();
@@ -17,8 +30,14 @@ async function filterBtnClick(event) {
   }
   try {
     getExercises(qwer).then(data => {
-      console.log(data);
-      markupExercises(data);
+      const { results, totalPages, page } = data;
+      markupExercises(results);
+      if (totalPages > 1) {
+        const pag = paginationPages(page, totalPages);
+        pagination.innerHTML = pag;
+      } else {
+        pagination.innerHTML = '';
+      }
     });
   } catch (error) {
     console.log(error);
@@ -30,11 +49,12 @@ async function getExercises(filter = filterValueDefault) {
     const response = await axios.get(`${BASE_URL}/filters`, {
       params: {
         filter: filter,
-        page: 1,
-        limit: 8,
+        page: currentPage,
+        limit: currentLimit,
       },
     });
-    return response.data.results;
+
+    return response.data;
   } catch (error) {
     console.log(error);
   }
@@ -43,14 +63,43 @@ async function getExercises(filter = filterValueDefault) {
 function markupExercises(results) {
   const markup = results
     .map(
-      ({ name, filter, imgUrl }) => ` <li data-filter>
+      ({ name, filter, imgUrl }) => ` <li class="filter-list" data-filter>
         <img class="img-exercises" src="${imgUrl}" alt="${filter}">
-        <div>
-          <p>${name}</p>
-          <p>${filter}</p>
+        <div class="filter-text">
+          <p class="filter-exercises">${name}</p>
+          <p class="filter-name">${filter}</p>
         </div>
       </li>`
     )
     .join('');
   exerciseFiltersList.insertAdjacentHTML('beforeend', markup);
 }
+
+function paginationPages(page, totalPages) {
+  let paginationHtml = '';
+  for (let i = 1; i <= totalPages; i += 1) {
+    paginationHtml += `<button class="pagination-btn" type="button">${i}</button>`;
+  }
+  return paginationHtml;
+}
+
+async function onPaginationPages(e) {
+  console.log(e.target.textContent);
+  console.log(currentPage);
+  currentPage = e.target.textContent;
+  exerciseFiltersList.innerHTML = '';
+  try {
+    const { results, page, totalPages } = await getExercises();
+    console.log(results);
+    console.log(page);
+    console.log(totalPages);
+    if (page === totalPages) {
+      return;
+    }
+    markupExercises(results);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+pagination.addEventListener('click', onPaginationPages);
