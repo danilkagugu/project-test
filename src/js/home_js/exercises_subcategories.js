c2d158c38a257b6fade48641ec67689e27762eb8
Danulo <danilyanishevski2001@gmail.com>
2024-02-12 12:20:20 +0200

    sadflcds

--- src/js/home_js/exercises_subcategories.js
+++ src/js/home_js/exercises_subcategories.js
@@ -164,7 +164,7 @@ function updateExercisesHeaderMarkup(nameValue) {
       <input class='SearchInput' name="search" placeholder="Search" type="search" id="search" />
       <button class='SearchButton' type="submit">
         <svg class='IconSearch' width='18' height='18'>
-          <use href='./img/symbol-defs.svg#icon-search'></use>
+          <use href='/img/symbol-defs.svg#icon-search'></use>
         </svg>
       </button>
     </form></div>
